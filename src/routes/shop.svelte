d1812db0bf52a40144a905dd0b332c9677db9515
miinm030 <miinm030@gmail.com>
2021-07-01 06:43:02 +0000

    修正

--- src/routes/shop.svelte
+++ src/routes/shop.svelte
@@ -6,7 +6,7 @@
   }
 </script>
 
-<nav class="w-full py-1 top-0 z-30 dark:bg-black">
+<nav class="w-full py-1 top-0 z-30">
   <div
     class="container flex flex-wrap mx-auto mt-0 w-full py-3 px-6 relative items-center justify-between"
   >
@@ -27,7 +27,7 @@
     <div class="order-3 w-full hidden md:flex md:items-center md:w-auto md:order-1" id="menu">
       <nav>
         <ul
-          class="text-base pt-4 text-gray-700 md:flex dark:text-white items-center justify-between md:pt-0"
+          class="text-base pt-4 text-gray-700 md:flex items-center justify-between md:pt-0"
           id="menu-list"
         >
           <li>
@@ -43,11 +43,11 @@
 
     <div class="order-1 md:order-2">
       <a
-        class="flex font-bold text-xl tracking-wide text-gray-800 dark:text-white items-center no-underline hover:no-underline"
+        class="flex font-bold text-xl tracking-wide text-gray-800 items-center no-underline hover:no-underline"
         href="/"
       >
         <svg
-          class="fill-current mr-2 text-gray-800 dark:text-white"
+          class="fill-current mr-2"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
@@ -61,8 +61,8 @@
       </a>
     </div>
 
-    <div class="flex order-2 text-gray-800 dark:text-white items-center md:order-3">
-      <a class="inline-block no-underline hover:text-black" href="#">
+    <div class="flex order-2 text-gray-800 items-center md:order-3">
+      <a class="inline-block no-underline" href="#">
         <svg
           class="fill-current hover:text-black"
           xmlns="http://www.w3.org/2000/svg"
@@ -103,8 +103,8 @@
   </div>
 </nav>
 
-<section class="bg-white py-8 dark:(bg-black text-white)">
-  <div class="container flex flex-wrap mx-auto pt-4 pb-12 items-center">
+<section class="bg-white py-8">
+  <div class="containermx-auto pt-4 pb-12 items-center">
     <nav class="w-full py-1 px-6 top-0 z-30">
       <div
         class="container flex flex-wrap mx-auto mt-0 w-full py-3 px-2 items-center justify-between"
@@ -113,7 +113,7 @@
           class="font-bold text-xl tracking-wide uppercase no-underline hover:no-underline"
           href="#"
         >
-          TOP(見本)
+          Store（実装用）
         </a>
 
         <div class="flex items-center">
@@ -146,13 +146,13 @@
       </div>
     </nav>
 
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+    <div class="p-6">
       <a href="#">
         <img
           class="hover:grow hover:shadow-lg"
           src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
         />
-        <div class="flex pt-3 items-center justify-between">
+        <div class="pt-3">
           <p class="">Product Name</p>
           <svg class="h-6 fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path
@@ -164,123 +164,15 @@
       </a>
     </div>
 
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+    <div class="p-6">
       <a href="#">
         <img
           class="hover:grow hover:shadow-lg"
           src="https://images.unsplash.com/photo-1508423134147-addf71308178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
         />
-        <div class="flex pt-3 items-center justify-between">
+        <div class="pt-3">
           <p class="">Product Name</p>
           <svg class="h-6 fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
-            />
-          </svg>
-        </div>
-        <p class="pt-1">£9.99</p>
-      </a>
-    </div>
-
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
-      <a href="#">
-        <img
-          class="hover:grow hover:shadow-lg"
-          src="https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
-        />
-        <div class="flex pt-3 items-center justify-between">
-          <p class="">Product Name</p>
-          <svg class="h-6 fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
-            />
-          </svg>
-        </div>
-        <p class="pt-1">£9.99</p>
-      </a>
-    </div>
-
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
-      <a href="#">
-        <img
-          class="hover:grow hover:shadow-lg"
-          src="https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
-        />
-        <div class="flex pt-3 items-center justify-between">
-          <p class="">Product Name</p>
-          <svg class="h-6 fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
-            />
-          </svg>
-        </div>
-        <p class="pt-1">£9.99</p>
-      </a>
-    </div>
-
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
-      <a href="#">
-        <img
-          class="hover:grow hover:shadow-lg"
-          src="https://images.unsplash.com/photo-1467949576168-6ce8e2df4e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
-        />
-        <div class="flex pt-3 items-center justify-between">
-          <p class="">Product Name</p>
-          <svg class="h-6 fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
-            />
-          </svg>
-        </div>
-        <p class="pt-1">£9.99</p>
-      </a>
-    </div>
-
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
-      <a href="#">
-        <img
-          class="hover:grow hover:shadow-lg"
-          src="https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
-        />
-        <div class="flex pt-3 items-center justify-between">
-          <p class="">Product Name</p>
-          <svg class="h-6 fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
-            />
-          </svg>
-        </div>
-        <p class="pt-1">£9.99</p>
-      </a>
-    </div>
-
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
-      <a href="#">
-        <img
-          class="hover:grow hover:shadow-lg"
-          src="https://images.unsplash.com/photo-1550837368-6594235de85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
-        />
-        <div class="flex pt-3 items-center justify-between">
-          <p class="">Product Name</p>
-          <svg class="h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
-            />
-          </svg>
-        </div>
-        <p class="pt-1">£9.99</p>
-      </a>
-    </div>
-
-    <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
-      <a href="#">
-        <img
-          class="hover:grow hover:shadow-lg"
-          src="https://images.unsplash.com/photo-1551431009-a802eeec77b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=400&q=80"
-        />
-        <div class="flex pt-3 items-center justify-between">
-          <p class="">Product Name</p>
-          <svg class="h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path
               d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z"
             />
@@ -292,7 +184,7 @@
   </div>
 </section>
 
-<section class="bg-white py-8 dark:(bg-black text-white)">
+<section class="bg-white py-8">
   <div class="container mx-auto py-8 px-6">
     <a
       class="font-bold text-xl mb-8 tracking-wide uppercase no-underline hover:no-underline"
